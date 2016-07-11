013e670f8548f11970510fa21afeaeb2b3b82afe
Reind Dooyeweerd <register@vout.nl>
2016-07-11 16:28:39 +0200

    selectize added

--- arctic/static/arctic/src/assets/js/app.js
+++ arctic/static/arctic/src/assets/js/app.js
@@ -120,4 +120,16 @@ $(document).ready(function() {
         });
     }
 
+    $('.selectize').selectize();
+
+    $('.selectize-tags').selectize({
+        delimiter: ',',
+        persist: false,
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    });
 });
