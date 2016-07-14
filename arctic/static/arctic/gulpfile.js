285dd9f3c6cef476bbb6b63f63832138d0cd1dcf
R4YM3 <raymond@hetwebbureau.nl>
2016-07-14 09:56:01 +0200

    Removed csso

--- arctic/static/arctic/gulpfile.js
+++ arctic/static/arctic/gulpfile.js
@@ -47,7 +47,6 @@ gulp.task('sass', function() {
       .pipe( $.autoprefixer( {
           browsers: config.autoprefixer.browsers
       } ) )
-      .pipe($.csso())
       // Todo: add build date
       .pipe( gulp.dest( config.path.build + config.path.css ) );
 });
