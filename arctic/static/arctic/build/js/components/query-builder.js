1d9302904339e63f50965e4442416784697752c1
R4YM3 <raymond@hetwebbureau.nl>
2016-07-21 08:52:56 +0200

    query builder by seperate js file

--- arctic/static/arctic/build/js/components/query-builder.js
+++ arctic/static/arctic/build/js/components/query-builder.js
@@ -1,37 +1,47 @@
 "use strict";
 
-( function() {
+( function () {
 
-    var name = '.rule-builder';
-    var element = $( name );
-
-    // get data
+    // required fields
+    var element = $( '.rule-builder' );
     var filters = element.data( 'filters' );
 
-    // init querybuilder
-    element.queryBuilder({
-        icons: {
+    if ( element.length && filters.length ) {
+
+        // settings
+        var settings = {};
+
+        settings.icons = {
             add_group: 'fa fa-plus-square',
             add_rule: 'fa fa-plus-circle',
             remove_group: 'fa fa-minus-square',
             remove_rule: 'fa fa-minus-circle',
             error: 'fa fa-exclamation-triangle'
-        },
-        allow_groups: 1,
-        filters: filters
-    });
-
-    // get form elements
-    var form = element.closest('form');
-    var submit = form.find('[type="submit"]');
-
-    // save rules
-    submit.on('click', function( event ) {
-        var result = element.queryBuilder('getRules');
-
-        if (!$.isEmptyObject(result)) {
-            $('#id_conditions').val(JSON.stringify(result, null, 2));
         }
-    });
+        settings.allow_groups = 1;
+        settings.filters = {};
+        settings.filters = filters;
 
-}) ();
+        var conditions = element.data( 'conditions' );
+        if ( conditions != {} ) {
+            settings.rules = conditions;
+        }
+
+        // init querybuilder
+        element.queryBuilder( settings );
+
+        // get form elements
+        var form = element.closest( 'form' );
+        var submit = form.find( '[type="submit"]' );
+
+        // save rules
+        submit.on( 'click', function ( event ) {
+            var result = element.queryBuilder( 'getRules' );
+
+            if ( !$.isEmptyObject( result ) ) {
+                $( '#id_conditions' ).val( JSON.stringify( result, null, 2 ) );
+            }
+        } );
+    }
+} )();
+
