fdb0624322e944e48e6646746c11c692f6bbd6c9
R4YM3 <raymond@hetwebbureau.nl>
2016-09-28 14:25:34 +0200

    updated design pattern

--- arctic/static/arctic/src/assets/js/components/sortable.js
+++ arctic/static/arctic/src/assets/js/components/sortable.js
@@ -8,23 +8,30 @@
 
     Optional
     * data-sort-handle - to specify a specific dragging handle
-    * <row data-sort-placeholder /> - field to save position of row (needs to be defined on row!)
+    * data-sort-placeholder - field to save position of row
 
     * data-delete-handle - delete button
     * data-delete-placeholder - field to check as delete
  */
 
-var sortable = {
-    element: $( '[data-sortable]' ),
-    rowClass: "",
-    row: "",
-    sortHandle: "",
-    sortPlaceholder: "",
-    deleteHandle: "",
-    deletePlaceholder: "",
+( function ( $, Sort ) {
 
-    init: function () {
-        var self = this;
+    'use strict'
+
+    function Sortable( element ) {
+        this.element = $( '[data-sortable]' )
+        this.rowClass = ""
+        this.row = ""
+        this.sortHandle = ""
+        this.sortPlaceholder = ""
+        this.deleteHandle = ""
+        this.deletePlaceholder = ""
+
+        self = this
+        self.init()
+    }
+
+    Sortable.prototype.init = function ( ) {
 
         // is there a sortable element?
         if ( self.element.size() ) {
@@ -48,13 +55,11 @@ var sortable = {
 
             // create a sortable list
             self.sorting();
-        };
-    },
+        }
+    }
 
 
-    sorting: function () {
-        var self = this
-
+    Sortable.prototype.sorting = function ( ) {
         // convert jquery element in javascript element
         let htmlElement = self.element.get(0);
 
@@ -74,16 +79,12 @@ var sortable = {
         }
 
         // init sortable
-        var sortable = Sortable.create( htmlElement, config );
+        var sortable = Sort.create( htmlElement, config );
     },
 
 
-    recalc: function () {
-        var self = this;
+    Sortable.prototype.recalc = function () {
         var items = $( self.rowClass );
-
-        // where to same positions?
-        self.sortPlaceholder = self.element.data( 'sort-placeholder' );
 
         // update positions
         items.each( function ( i, el ) {
@@ -106,7 +107,7 @@ var sortable = {
     },
 
 
-    remove: function ( el, self ) {
+    Sortable.prototype.remove = function ( el, self ) {
         let element = $( el );
         let rule = element.closest( self.rowClass );
         let ruleWrapper = rule.parent();
@@ -125,6 +126,9 @@ var sortable = {
         // recalc positions
         self.recalc();
     }
-}
 
-sortable.init()
+
+    // initiate
+    new Sortable()
+
+})( jQuery, Sortable );
