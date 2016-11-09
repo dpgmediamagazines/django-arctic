4b98c0fa18609a046920f8127973b10a397372a8
R4YM3 <raymond@hetwebbureau.nl>
2016-11-09 11:32:43 +0100

    semicolon's

--- arctic/static/arctic/src/assets/js/components/reveal-iframe.js
+++ arctic/static/arctic/src/assets/js/components/reveal-iframe.js
@@ -12,28 +12,28 @@
     'use strict';
 
     function RevealIframe( element ) {
-        this.element = element
+        this.element = element;
 
         // setup dialog with iframe listener
         if ( this.element && this.element.size() ) {
-            this.location = window.location.href
-            this.id = this.element.data( 'open' )
-            this.dialog = $( '#' + this.id )
-            this.iframe = this.dialog.find( 'iframe' )
+            this.location = window.location.href;
+            this.id = this.element.data( 'open' );
+            this.dialog = $( '#' + this.id );
+            this.iframe = this.dialog.find( 'iframe' );
         }
 
         // it's an iframe
         if ( window.parent != window ) {
             this.framed = true;
             this.parent = window.parent;
-            this.body = $( 'body' )
-            this.auto_close = this.element.data( 'auto-close' )
-            this.parent_reload = $('body').data( 'refresh-parent' )
-            this.hide = $( '[data-close]' )
+            this.body = $( 'body' );
+            this.auto_close = this.element.data( 'auto-close' );
+            this.parent_reload = $('body').data( 'refresh-parent' );
+            this.hide = $( '[data-close]' );
         }
 
-        var self = this
-        this.init()
+        var self = this;
+        this.init();
     }
 
 
@@ -45,12 +45,12 @@
             this.element.on( 'click', function ( event ) {
                 event.preventDefault();
 
-                self.url = $(this).data( 'url' )
-                self.id = $(this).data( 'open' )
-                self.size = $(this).data( 'size' )
+                self.url = $(this).data( 'url' );
+                self.id = $(this).data( 'open' );
+                self.size = $(this).data( 'size' );
 
                 if ( this.size ) {
-                    this.dialog.addClass( this.size )
+                    this.dialog.addClass( this.size );
                 }
 
                 self.open( self.url );
@@ -65,11 +65,11 @@
             }
 
             if ( this.auto_close ) {
-                this.close( )
+                this.close( );
             }
 
             this.hide.on( 'click' , function ( ) {
-                self.close( )
+                self.close( );
             })
         }
     }
@@ -79,7 +79,7 @@
         // setup iframe
 
         self.dialog.on( 'open.zf.reveal', function ( ) {
-            self.iframe.attr( 'src', url )
+            self.iframe.attr( 'src', url );
 
             self.iframe.on( 'load' , function( ) {
                 this.style.height = this.contentDocument.body.scrollHeight +'px';
@@ -98,7 +98,7 @@
         // close all reveal dialogs within parent window
         this.dialog.trigger('closeme.zf.reveal');
 
-        this.iframe = this.dialog.find('iframe')
+        this.iframe = this.dialog.find('iframe');
 
         // disable areYouSure in iframe before removing src
         this.form = this.iframe.contents().find( '.dirty-check' );
@@ -111,6 +111,6 @@
 
 
     // initiate
-    new RevealIframe( $( '[data-open][data-url], [data-open][data-auto-close]' ) )
+    new RevealIframe( $( '[data-open][data-url], [data-open][data-auto-close]' ) );
 
 })( jQuery );
