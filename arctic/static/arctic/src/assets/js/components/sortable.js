8857461569a14b0237121fd17c8afafeafbd6c69
R4YM3 <raymond@hetwebbureau.nl>
2016-11-09 11:11:12 +0100

    added missing var statement, added missing semicolons and added comments

--- arctic/static/arctic/src/assets/js/components/sortable.js
+++ arctic/static/arctic/src/assets/js/components/sortable.js
@@ -19,31 +19,32 @@
     'use strict'
 
     function Sortable( element ) {
-        this.element = $( '[data-sortable]' )
-        this.rowClass = ""
-        this.row = ""
-        this.sortHandle = ""
-        this.deleteHandle = ""
-        this.deletePlaceholder = ""
+        this.element = $( '[data-sortable]' );
+        this.rowClass = "";
+        this.row = "";
+        this.sortHandle = "";
+        this.deleteHandle = "";
+        this.deletePlaceholder = "";
 
-        self = this
-        self.init()
+        var self = this;
+        self.init();
     }
 
     Sortable.prototype.init = function ( ) {
+        var self = this;
 
-        // is there a sortable element?
+        // is there a sortable element, if so gather information about setup
         if ( self.element.size() ) {
             self.rowClass = self.element.data( 'row' );
             self.row = self.element.find( self.rowClass );
 
             // error handling, is there something to sort?
             if ( !self.row.size() ) {
-                console.log( 'Sortable: data-to-sort not valid or empty' )
+                console.log( 'Sortable: data-to-sort not valid or empty' );
                 return
             }
 
-            // optional delete
+            // check if there's a delete button, if so listen to it
             self.deleteHandle = self.row.find( self.element.data( 'delete-handle' ) );
             if ( self.deleteHandle.size() ) {
 
@@ -52,22 +53,25 @@
                 });
             }
 
-            // create a sortable list
+            // initiate sortable list
             self.sorting();
         }
     }
 
 
+    // initate sorting
     Sortable.prototype.sorting = function ( ) {
+        var self = this;
+
         // convert jquery element in javascript element
-        let htmlElement = self.element.get(0);
+        var htmlElement = self.element.get(0);
 
         // sortable config
         var config = {
             animation: 200,
             onUpdate: function ( event ) {
                 // recalc positions when sorting is updated
-                self.recalc()
+                self.recalc();
             }
         }
 
@@ -82,14 +86,16 @@
     },
 
 
+    // recalc positions of sortable elements
     Sortable.prototype.recalc = function () {
+        var self = this;
         var items = $( self.rowClass );
 
         // update positions
         items.each( function ( i, el ) {
-            let element = $( el );
-            let index = parseInt( items.index( element ) );
-            let placeholder = element.data('sort-placeholder')
+            var element = $( el );
+            var index = parseInt( items.index( element ) );
+            var placeholder = element.data('sort-placeholder')
 
             // find placeholder within row if not exist check outside row
             placeholder = element.find( placeholder );
@@ -105,11 +111,11 @@
         });
     },
 
-
+    // removes a row, by checking the delete checkbox
     Sortable.prototype.remove = function ( el, self ) {
-        let element = $( el );
-        let rule = element.closest( self.rowClass );
-        let ruleWrapper = rule.parent();
+        var element = $( el );
+        var rule = element.closest( self.rowClass );
+        var ruleWrapper = rule.parent();
 
         // flag for deletion if there's a placeholder
         self.deletePlaceholderClass = self.element.data( 'delete-placeholder' );
@@ -128,6 +134,6 @@
 
 
     // initiate
-    new Sortable()
+    new Sortable();
 
 })( jQuery, Sortable );
