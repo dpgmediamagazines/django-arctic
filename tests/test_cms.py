29facd6a6fbf33a9aaf545e8e5aece9071fd8d49
Arjan Peddemors <arjan@aquilis.nl>
2016-11-28 11:30:51 +0100

    Test login redirect

--- tests/test_cms.py
+++ tests/test_cms.py
@@ -12,6 +12,57 @@ class TestCMS(object):
     def init(self):
         self.__class__.client = Client()
 
+    def test_login_success_redirect_no_next(self, admin_user):
+        """
+        Asserts that an admin user is redirected to root after login,
+        when no 'next' query param is given.
+
+        :param admin_user: fixture provided by pytest-django
+        :return: void
+        """
+        response = self.client.post('/login/',
+                                    {
+                                     'username': 'admin',
+                                     'password': 'password'
+                                    })
+
+        assert response.status_code == 302
+        assert response.url == '/'
+
+    def test_login_success_redirect_next(self, admin_user):
+        """
+        Asserts that an admin user is redirected to the next page after
+        login, when a 'next' query param is given.
+
+        :param admin_user: fixture provided by pytest-django
+        :return: void
+        """
+        response = self.client.post('/login/?next=/articles/',
+                                    {
+                                     'username': 'admin',
+                                     'password': 'password'
+                                    })
+
+        assert response.status_code == 302
+        assert response.url == '/articles/'
+
+    def test_login_success_redirect_next_other_host(self, admin_user):
+        """
+        Asserts that an admin user is redirected to root after login,
+        when the 'next' query param refers to another host.
+
+        :param admin_user: fixture provided by pytest-django
+        :return: void
+        """
+        response = self.client.post('/login/?next=http://www.no.domain/test/',
+                                    {
+                                     'username': 'admin',
+                                     'password': 'password'
+                                    })
+
+        assert response.status_code == 302
+        assert response.url == '/'
+
     def test_login_success(self, admin_user):
         """
         Asserts that an admin user can log in an see a restricted page
