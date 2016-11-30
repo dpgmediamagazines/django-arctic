d4acc8cf63ba027901f03694162e286efa3b2974
R4YM3 <raymond@hetwebbureau.nl>
2016-11-30 10:46:25 +0100

    added getCookie helper

--- arctic/static/arctic/src/assets/js/utils/helpers.js
+++ arctic/static/arctic/src/assets/js/utils/helpers.js
@@ -1,6 +1,25 @@
+//TODO: nest these helpers within one object, like arctic..
+
 function slugify( Text ) {
     return Text
         .toLowerCase()
         .replace( /[^\w ]+/g, '' )
         .replace( / +/g, '-' );
+}
+
+// getCookie helper
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
