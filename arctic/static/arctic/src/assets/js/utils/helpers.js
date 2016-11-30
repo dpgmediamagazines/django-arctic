b2c2f1e3cf562c9fa4691e8ed9aa4f132ca949d3
R4YM3 <raymond@hetwebbureau.nl>
2016-11-30 22:14:36 +0100

    global utils in arctic object

--- arctic/static/arctic/src/assets/js/utils/helpers.js
+++ arctic/static/arctic/src/assets/js/utils/helpers.js
@@ -2,41 +2,44 @@
 
 // global var whichs all public data, helpers etc..
 window.arctic = {};
-window.arctic.utils = {};
+window.arctic.utils = {
 
+    getCookie: function ( name ) {
+        var cookieValue = null;
 
-function slugify( Text ) {
-    return Text
-        .toLowerCase()
-        .replace( /[^\w ]+/g, '' )
-        .replace( / +/g, '-' );
-}
+        if ( document.cookie && document.cookie !== '' ) {
+            var cookies = document.cookie.split( ';' );
 
-// getCookie helper
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
+            for ( var i = 0; i < cookies.length; i++ ) {
+                var cookie = jQuery.trim( cookies[i] );
+
+                // Does this cookie string begin with the name we want?
+                if ( cookie.substring( 0, name.length + 1 ) === ( name + '=' ) ) {
+                    cookieValue = decodeURIComponent( cookie.substring( name.length + 1 ));
+                    break;
+                }
             }
-        }
+        };
+
+        return cookieValue;
+    },
+
+    growl: function ( type, title, text ) {
+        var growl = '<div class="growl ' + type + '"> \
+            <h6>' + title + '</h6> \
+            <p>' + text + '</p> \
+        </div>';
+
+        var growl = $( growl );
+        $( 'body' ).append( growl );
+
+        return growl;
+    },
+
+    slugify: function ( text ) {
+        return text
+            .toLowerCase()
+            .replace( /[^\w ]+/g, '' )
+            .replace( / +/g, '-' );
     }
-    return cookieValue;
-}
-
-window.arctic.utils.growl = function( type, title, text ) {
-    var growl = '<div class="growl ' + type + '"> \
-        <h6>' + title + '</h6> \
-        <p>' + text + '</p> \
-    </div>';
-
-    var growl = $( growl );
-    $( 'body' ).append( growl );
-
-    return growl;
-}
+};
