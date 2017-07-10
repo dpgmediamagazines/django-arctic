82b6c3e61700c3f57466b4ce0b1566ffa09387ae
Jose Moreira <jose.moreira@sanoma.com>
2017-07-10 14:48:34 +0200

    Add Arctic customizations to template of new project

--- arctic/project_template/project_name/settings.py
+++ arctic/project_template/project_name/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'arctic',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Arctic configuration
+ARCTIC_SITE_NAME = "{{ project_name }}'s Arctic website"
