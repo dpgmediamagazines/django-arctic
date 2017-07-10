14a0b7379c84fdc15dba4c77ab99c3927fc6082b
Johnny Mijnhout <johnny.mijnhout@sanoma.com>
2017-07-10 19:49:21 +0200

    updated example with advanced_search_form

--- example/articles/forms.py
+++ example/articles/forms.py
@@ -3,6 +3,7 @@ from __future__ import (absolute_import, unicode_literals)
 from django import forms
 
 from .models import Article
+from django.db.models import Q
 
 
 class ArticleForm(forms.ModelForm):
@@ -14,3 +15,13 @@ class ArticleForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ArticleForm, self).__init__(*args, **kwargs)
         self.fields['category'].empty_label = ""
+
+
+class AdvancedCategorySearchForm(forms.Form):
+    name = forms.CharField(max_length=100, required=False, label='Strict name match')
+
+    def get_search_filter(self):
+        value = self.data.get('name')
+        if value:
+            return Q(name=value)
+        return Q()
