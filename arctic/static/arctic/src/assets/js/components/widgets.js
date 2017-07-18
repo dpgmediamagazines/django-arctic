66bacad3828a62068745a0796ce8ab28272325f5
Miguel Marcos <mmarcos@rodesia.org>
2017-07-18 10:07:08 +0200

    refactored widgets javascript

---
+++ arctic/static/arctic/src/assets/js/components/widgets.js
@@ -0,0 +1,109 @@
+// convert date format specification from the django/php syntax to the js
+// datepicker spec
+function django2datepicker(django_format) {
+    var translation_dict = {
+        '%': '',
+        'y': 'yy',
+        'Y': 'yyyy',
+        'm': 'mm',
+        'd': 'dd',
+        'H': 'hh',
+        'I': 'hh',
+        'M': 'ii',
+        'p': 'aa',
+        'S': ''
+    }
+
+    var datepicker_format = '';
+    for (var i = 0, len = django_format.length; i < len; i++) {
+        if (django_format[i] in translation_dict) {
+            datepicker_format += translation_dict[django_format[i]];
+        }
+        else {
+            datepicker_format += django_format[i];
+        }
+    }
+
+    if (datepicker_format.slice(-1) == ':') {
+        datepicker_format = datepicker_format.slice(0, -1);
+    }
+    return datepicker_format;
+}
+
+
+$(document).ready(function() {
+    $('[js-selectize]').each(function(index) {
+        $(this).selectize({
+            allowEmptyOption: true,
+            highlight: false,
+            plugins: ['remove_button']
+        });
+    });
+
+    $('[js-selectize-multiple]').each(function(index) {
+        $(this).selectize({
+            delimiter: ',',
+            persist: false,
+            plugins: ['remove_button'],
+            create: function(input) {
+                return {
+                    value: input,
+                    text: input
+                }
+            }
+        });
+    });
+
+    $('[js-selectize-autocomplete]').each(function(index) {
+        var url = $(this).attr('data-url');
+        $(this).selectize({
+            valueField: 'value',
+            labelField: 'label',
+            searchField: 'label',
+            create: false,
+            load: function(query, callback) {
+                if (!query.length) return callback();
+                $.ajax({
+                    url: url + encodeURIComponent(query),
+                    type: 'GET',
+                    error: function() {
+                        callback();
+                    },
+                    success: function(res) {
+                        callback(res.options);
+                    }
+                });
+            }
+        });
+    });
+
+    $('[js-datepicker]').each(function(index) {
+        $(this).datepicker({
+            todayButton: true,
+            language: 'en',
+            startDate: new Date($(this).attr("data-date")),
+            dateFormat: django2datepicker(DATE_FORMAT)
+        });
+    });
+
+    $('[js-timepicker]').each(function(index) {
+        $(this).datepicker({
+            onlyTimepicker: true,
+            language: 'en',
+            startDate: new Date($(this).attr("data-time")),
+            timeFormat: django2datepicker(TIME_FORMAT),
+            timepicker: true
+        });
+    });
+
+    $('[js-datetimepicker]').each(function(index) {
+        $(this).datepicker({
+            language: 'en',
+            todayButton: true,
+            startDate: new Date($(this).attr("data-datetime")),
+            dateFormat: django2datepicker(DATE_FORMAT),
+            timeFormat: django2datepicker(TIME_FORMAT),
+            timepicker: true
+        });
+    });
+});
