d4e45636a3a3c1bddcfbf3dd00297d914df6ea58
David Cunha Esteves <d_c_esteves@hotmail.com>
2017-09-22 13:37:26 +0200

    Dismiss popover on click outside div

--- arctic/static/arctic/src/assets/js/components/toggle_form.js
+++ arctic/static/arctic/src/assets/js/components/toggle_form.js
@@ -12,4 +12,20 @@
         },
         html: true
     });
+
+
+    var dismissOnClick = function () {
+        $('body').on('click.popover', function(e) {
+            var target = e.target;
+            if (!$(target).is('.popover') &&
+                !$(target).parents().is('.popover') &&
+                !$(target).is('.arctic_toggle_form_button') &&
+                !$(target).parents().is('.arctic_toggle_form_button')
+                ) {
+                $('body').off('click.popover');
+                $('.arctic_toggle_form_button').popover('hide');
+            }
+        });
+    }
+    $('.arctic_toggle_form_button').on('show.bs.popover', dismissOnClick);
 })(jQuery);
