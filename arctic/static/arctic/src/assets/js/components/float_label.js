693746d5bdd7ce7948c21b363be795c52db011ed
Miguel Marcos <mmarcos@rodesia.org>
2018-02-13 12:02:55 +0100

    added dynamic 'empty' class to inputs to improve detection of when they are empty and style accordingly

--- arctic/static/arctic/src/assets/js/components/float_label.js
+++ arctic/static/arctic/src/assets/js/components/float_label.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
+    $('.float-label input, .float-label textarea').each(function() {
+        if (($(this).val().length == 0) && ($(this).text() == 0)) {
+            $(this).addClass('empty');
+        }
+    });
+    
     $('.float-label input, .float-label textarea').blur(function() {
         // Check if float label input is not empty, if so then add some CSS
         if (($(this).val().length > 0) || ($(this).text() > 0)) {
             $(this).removeClass('empty');
-            alert('not empty');
         }
         else {
             $(this).addClass('empty');
-            alert('empty');
         }
     });
     
-    // Check if float label input has items, if so then add some CSS
-    if ($('.float-label .selectize-input').find('.item').length > 0) {
-        $('.selectize-input').parent().next('label').css({
-            top: '.3rem',
-            fontSize: '75%'
-        });
+    // Selectize controls
+    $('.float-label .selectize-input').each(function() {
+        if $(this).find('.item').length == 0) {
+            $(this).addClass('empty');
+        }
     }
+    $('.float-label .selectize-input').blur(function() {
+        if $(this).find('.item').length > 0) {
+            $(this).removeClass('empty');
+        }
+        else {
+            $(this).addClass('empty');
+        }
+    }
+
+
 });
