40fccce8c6ac36b590d178154a9cd196f2a5ca5f
David Esteves <david.esteves@sanoma.com>
2019-08-21 12:41:20 +0200

    typo

--- arctic/project_template/start/config/settings.py
+++ arctic/project_template/start/config/settings.py
@@ -110,7 +110,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en2.2/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
 
